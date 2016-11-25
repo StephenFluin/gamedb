f21b357727825b123430ca5212535c685dbcd551
Stephen Fluin <stephen.fluin@gmail.com>
2016-11-25 11:17:50 -0800

    Allow field edit

--- src/app/my-list.component.ts
+++ src/app/my-list.component.ts
@@ -15,24 +15,29 @@ import 'rxjs/add/operator/switchMap';
                     <div *ngIf="game.peakYear">{{game.peakYear}}</div>
                     <div *ngIf="game.hours">{{game.hours}}h estimated</div>
                 </md-card-content>
-                <md-card-actions><button md-raised-button (click)="delete(game)">x</button></md-card-actions>
+                <md-card-actions>
+                    <button md-raised-button (click)="edit(game)">Edit</button>
+                    <button md-raised-button (click)="delete(game)">x</button>
+                </md-card-actions>
 
             </md-card>
         </div>
         <div *ngIf="(gameList | async) === null">Loading games...</div>
         <div *ngIf="(gameList | async) && (gameList | async).length == 0">You have no games in your db yet.</div>
 
-        <form #newGame="ngForm" (submit)="create(newGame.value)">
-            <md-input name="name" ngModel placeholder="Game Name"></md-input>
-            <md-input name="peakYear" ngModel placeholder="Peak Year" type="number"></md-input>
-            <md-input name="hours" ngModel placeholder="Estimated Hours" type="number"></md-input>
+        <form #newGame="ngForm" (submit)="create(newGame)">
+            <input type="hidden" name="$key" [ngModel]="gameEdit.$key">
+            <md-input name="name" [ngModel]="gameEdit.name" placeholder="Game Name"></md-input>
+            <md-input name="peakYear" [ngModel]="gameEdit.peakYear" placeholder="Peak Year" type="number"></md-input>
+            <md-input name="hours" [ngModel]="gameEdit.hours" placeholder="Estimated Hours" type="number"></md-input>
             <button md-raised-button color="primary" type="submit">Create</button>
         </form>
     `,
 })
 export class MyListComponent {
     gameList: FirebaseListObservable<any[]>;
-    newGame : FormGroup;
+    gameEdit = {name: null, peakYear: null, hours: null, $key: null};
+
     constructor(public af: AngularFire, router: Router) {
         af.auth.subscribe(authState => {
             if (!authState) {
@@ -46,11 +51,23 @@ export class MyListComponent {
             }
         });
     }
-    create(newGame) {
+    create(newGame: FormGroup) {
         event.preventDefault();
-        let result = this.gameList.push(newGame);
+        if(newGame.value.$key) {
+            let key = newGame.value.$key;
+            delete newGame.value.$key;
+            this.gameList.update(key, newGame.value);
+        } else {
+            delete newGame.value.$key;
+            let result = this.gameList.push(newGame.value);
+        }
+        newGame.reset();
+        this.gameEdit = {name: null, peakYear: null, hours: null, $key: null};
     }
     delete(game) {
         this.gameList.remove(game);
+    }
+    edit(game) {
+        this.gameEdit = game;
     }
 }
